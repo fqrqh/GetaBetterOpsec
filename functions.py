2f52d01e2f940b9ab2ce88c5c05001c125d0db4b
fqrqh <fisousiphone@gmail.com>
2026-06-03 00:07:26 +0200

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,26 @@
+    def exitbtn(self):
+        exit()
+    
+    def tweaks(self):
+       print("jd")
+         
+    
+    def remove(self):
+        print("qd")
+    
+    def Show(self):
+        print("jdz")
+
+    def restore(self):
+        print("jdz")
+
+    def cleanup(self):
+        exit()
+    
+    def locationtrack(self):
+       print("jd")
+         
+    
+    def activity(self):
+        print("qd")
+    
